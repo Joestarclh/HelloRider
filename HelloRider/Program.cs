038e0ee6e7a693bece7d3f7862708f2f3a562b63
Joestarclh <82345538+Joestarclh@users.noreply.github.com>
2021-04-21 13:35:09 +0800

    Create README.md

---
+++ HelloRider/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloRider
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var fant = new DeskFan(new PowerSupply());
+            Console.WriteLine(fant.Work());
+        }
+
+    }
+
+    interface IPowerSupply
+    {
+        int GetPower();
+    }
+
+
+    class PowerSupply:IPowerSupply
+    {
+        public int GetPower()
+        {
+            return 100;
+        }
+    }
+    class DeskFan
+    {
+        private IPowerSupply _powerSupply;
+        public DeskFan(IPowerSupply powerSupply)
+        {
+            _powerSupply = powerSupply;
+        }
+        public string Work()
+        {
+            int power = _powerSupply.GetPower();
+            if (power <= 0)
+            {
+                return "won't work";
+            }
+            else if (power < 100)
+            {
+                return "slow";
+            }
+            else if (power < 200)
+            {
+                return "Work fine";
+            }
+            else
+            {
+                return "Warning";
+            }
+
+        }
+
+    }
+
+
+
+
+
+    }
